6b30c452d95c2c93cc6bbf207fdcd63a7dd12f45
freaky cheesy <92267915+freakycheesy@users.noreply.github.com>
2025-12-11 22:13:25 +0000

    init

---
+++ mods.js
@@ -0,0 +1,10 @@
+var downloadElement = document.getElementsByClassName("download");
+
+function downloadBegin(link, name) {
+    const a = document.createElement("a");
+    a.href = link;
+    a.download = name;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
